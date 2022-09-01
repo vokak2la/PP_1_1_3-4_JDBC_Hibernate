236d08ee74cdce2d357675784379bf66ac4b01cd
vokak2la <spectula84@mail.ru>
2022-09-01 12:27:24 +0300

    commit 1: Проверяем соединение с MySql WorkBench - работает!

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,14 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+
 public class Main {
-    public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+    public static void main(String[] args) throws SQLException {
+      Util util = new Util();
+      util.OpenConnection ("jdbc:mysql://127.0.0.1:3306/my_db","bestuser" , "bestuser" );
+      util.CloseConnection();
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,44 @@
 package jm.task.core.jdbc.util;
 
+import java.security.SecureRandom;
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    Connection connection;
+
+    Driver driver;
+
+    public void OpenConnection(String url, String username, String pass) throws SQLException {
+        try {
+            driver = new com.mysql.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+        } catch (SQLException ex) {
+            System.out.println("Error create driver");
+            return;
+        }
+
+        try {
+            connection = DriverManager.getConnection(url, username, pass);
+            if (!connection.isClosed()) {
+                System.out.println("Connection - good!");
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error connection");
+        }
+    }
+
+    public void CloseConnection() {
+        try {
+            if (!connection.isClosed()) {
+                connection.close();
+                System.out.println("Connection - closed!");
+            }
+        } catch (SQLException sqlException) {
+            System.out.println("Cannot close connection");
+        }
+    }
+
 }
